b1b4ec9ccc5d62bb5ee5a61f4b37d061803571aa
DrApfelStrudel <Chrischzz1@hotmail.com>
2014-12-04 19:43:02 +0100

    initial

---
+++ index.js
@@ -0,0 +1,105 @@
+
+/**
+ * @name DrApfelStrudel
+ * @module Sounds
+ */
+ 
+ export function sine(t, n) {
+   return Math.sin(t * note(n) * Math.PI);
+ }
+ 
+ export function square(t, n) {
+   var sineVal = sine(t, n);
+   if (sineVal >= 0) return 1
+   return -1;
+ }
+ 
+ export function Sound(start, end, syn, not, volume) {
+  var synth = syn;
+  var vol = volume || 0;
+  var note = not || 0;
+  
+  function play(t) {
+    var shouldPlay = start <= t && t <= end;
+    if (!shouldPlay || !synth) return 0;
+    
+    return synth(t, note) * vol; 
+  }
+  
+  function getStart() {
+    return start;
+  }
+  
+  function getEnd() {
+    return end;
+  }
+  
+  function setSynth(syn) {
+    synth = syn;
+  }
+  
+  function setNote(n) {
+    note = n;
+  }
+  
+  function setVol(v) {
+    vol = v;
+  }
+  
+  this.setVol = setVol;
+  this.play = play;
+  this.getStart = getStart;
+  this.getEnd = getEnd;
+  this.setSynth = setSynth;
+  this.setNote = setNote;
+}
+ 
+export function Track() {
+  var mainVol = 1;
+  var sounds = [];
+  var speed = 1;
+  
+  function addSounds(sounds) {
+    for (var i = 0; i < sounds.length; i++) {
+      addSound(sounds[i]);
+    }
+  }
+  
+  function addSound(sound) {
+    for (var i = Math.floor(sound.getStart()); i < sound.getEnd(); i++) {
+      if (sounds[i]) {
+        sounds[i].push(sound);
+      }
+      else {
+        sounds[i] = [sound];
+      }
+    }
+  }
+  
+  function play(t) {
+    var timeSound = sounds[Math.floor(t)];
+    var t = t * speed;
+    if (timeSound) {
+      var sum = 0;
+      for (var i = 0; i < timeSound.length; i++) {
+        sum += timeSound[i].play(t);
+      }
+      return sum * mainVol;
+    }
+    else {
+      return 0;
+    }
+  }
+  
+  function setSpeed(s) {
+    speed = s;
+  }
+  
+  this.play = play;
+  this.addSounds = addSounds;
+  this.setSpeed = setSpeed;
+}
+
+function note(n) {
+  return 440 * Math.pow (Math.pow (2, 1/12), n);
+}
---
+++ test.js
@@ -0,0 +1,39 @@
+
+/**
+ * @DrApfelStrudel
+ * Track environment
+ */
+
+import { Sound, Track, sine, square } from "./index";
+
+var t1 = new Track();
+
+var synthNodes = [7, 3, 7, 10, 7, 3, 0, -2
+                 ,0, 3, 7, 10, 7, 3, 0, -2
+                 ,0, 2, 3, 8, 7, 3, 2, 0
+                 ,-2, 0, 3, 7, 10, 12, 15, 12];
+                 
+var bassNodes = [-24, -24, -12, -12, -24, -24, -12, -12
+                ,-24, -24, -12, -12, -24, -24, -12, -12
+                ,-28, -28, -16, -16, -28, -28, -16, -16
+                ,-26, -26, -14, -14, -26, -26, -14, -14];
+                
+var synthSounds = [];
+var bassSounds = [];
+
+for (var i = 0; i < synthNodes.length; i++) {
+  var s = new Sound(i/8, i/8 + 0.125, square, synthNodes[i], 0.25);
+  synthSounds.push(s);
+}
+
+for (var i = 0; i < bassNodes.length; i++) {
+  var s = new Sound(i/8, i/8 + 0.125, square, bassNodes[i], 0.25);
+  bassSounds.push(s);
+}
+
+t1.addSounds(synthSounds);
+t1.addSounds(bassSounds);
+
+export function dsp(t) {
+  return t1.play(t%4);
+}
